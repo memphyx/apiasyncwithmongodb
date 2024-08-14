db0a8690ee6955567e7b0f8adf6ef03843cfb0e4
PC-USER <agnerojunior7390@gmail.com>
2024-08-14 19:18:50 +0000

    14/08/2024 19h18

--- auth/jwt_bearer.py
+++ auth/jwt_bearer.py
@@ -1,4 +1,4 @@
-from fastapi import Request, HTTPException
+    from fastapi import Request, HTTPException
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
 from auth.jwt_handler import decode_jwt
